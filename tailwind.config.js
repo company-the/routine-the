724e9e514e9ae646c331112b6cdc5141493fcd74
Roman Kondratyuk <roman.kondr505@gmail.com>
2024-09-07 20:36:56 +0300

    feat: Project init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './App.tsx',           // Main app file
+    './src/**/*.tsx',       // Scan all .tsx files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
